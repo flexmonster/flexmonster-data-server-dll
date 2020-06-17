595cef9c4e49cbe789de0ded48e18306e974d888
olena <olena.krysan@gmail.com>
2020-06-17 16:16:03 +0300

    Remove nulls from responses

--- DemoDataServerCore/Startup.cs
+++ DemoDataServerCore/Startup.cs
@@ -27,7 +27,10 @@ namespace DemoDataServerCore
             services.AddFlexmonsterApi();
             //custom parser must be added as transient
             services.AddTransient<IParser, CustomParser>();
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            }); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
