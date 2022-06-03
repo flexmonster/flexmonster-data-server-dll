eea601a68aa138e61853cc5d2b91e2fbba99b81f
solja <solja484@gmail.com>
2022-06-03 18:44:26 +0300

    remove ReloadService from HomeConsroller

--- DemoDataServerCore/Controllers/HomeController.cs
+++ DemoDataServerCore/Controllers/HomeController.cs
@@ -4,20 +4,8 @@ namespace DemoDataServerCore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ReloadService reloadService;
-
-        public HomeController(ReloadService rs)
-        {
-            reloadService = rs;
-        }
 		public IActionResult Index() 
         {   
-            return View();
-        }
-
-        public IActionResult Reload()
-        {
-            System.Threading.Tasks.Task task = reloadService.Reload("custom-index");
             return View();
         }
     }
